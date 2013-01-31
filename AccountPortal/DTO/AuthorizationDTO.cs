502db17c285f7e3855a6dcd45409b1cd76b6931d
kmiryalkar <kiran@thrinaina.com>
2013-01-31 21:06:58 +0530

    kiran

---
+++ AccountPortal/DTO/AuthorizationDTO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Kiran.Accountportal.Common;
+using System.Data;
+namespace Kiran.Accountportal.DTO
+{
+    public class AuthorizationDTO:BaseDTO
+    {
+        
+        private KeyObject reportsToUserKey;
+
+        public AuthorizationDTO(String _currentUserID)
+            : base(_currentUserID)
+        {
+            reportsToUserKey = new KeyObject();
+        }
+        public int UserRid { get; set; }
+        public String Password { get; set; }
+        public String Name { get; set; }
+        public String SurName { get; set; }
+        public String LastLoginDate { get; set; }
+        public KeyObject ReportsToUserKey { get; set; }
+        public DataSet UsersList { get; set; }
+        private Authority getAuthFromInt(String value)
+        {
+            switch (value)
+            {
+                case "0": return Authority.None;
+                case "1": return Authority.Deny;
+                case "2": return Authority.Allow;
+                case "3": return Authority.ReadOnly;
+                case "4": return Authority.Modify;
+                case "5": return Authority.Delete;
+            }
+            return Authority.None;
+        }
+    }
+}
