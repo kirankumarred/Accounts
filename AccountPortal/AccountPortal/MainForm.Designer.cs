73691eb38358d277606882fcb0781972bce3ae0f
kmiryalkar <kiran@thrinaina.com>
2013-01-31 21:32:30 +0530

    vg

--- AccountPortal/AccountPortal/MainForm.Designer.cs
+++ AccountPortal/AccountPortal/MainForm.Designer.cs
@@ -28,20 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.ultToolsbarmgr = new Infragistics.Win.UltraWinToolbars.UltraToolbarsManager(this.components);
+            this.MainForm_Fill_Panel = new Infragistics.Win.Misc.UltraPanel();
+            this._MainForm_Toolbars_Dock_Area_Left = new Infragistics.Win.UltraWinToolbars.UltraToolbarsDockArea();
+            this._MainForm_Toolbars_Dock_Area_Right = new Infragistics.Win.UltraWinToolbars.UltraToolbarsDockArea();
+            this._MainForm_Toolbars_Dock_Area_Top = new Infragistics.Win.UltraWinToolbars.UltraToolbarsDockArea();
+            this._MainForm_Toolbars_Dock_Area_Bottom = new Infragistics.Win.UltraWinToolbars.UltraToolbarsDockArea();
+            ((System.ComponentModel.ISupportInitialize)(this.ultToolsbarmgr)).BeginInit();
+            this.MainForm_Fill_Panel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // ultToolsbarmgr
+            // 
+            this.ultToolsbarmgr.DesignerFlags = 1;
+            this.ultToolsbarmgr.DockWithinContainer = this;
+            this.ultToolsbarmgr.DockWithinContainerBaseType = typeof(System.Windows.Forms.Form);
+            this.ultToolsbarmgr.Ribbon.Visible = true;
+            // 
+            // MainForm_Fill_Panel
+            // 
+            this.MainForm_Fill_Panel.Cursor = System.Windows.Forms.Cursors.Default;
+            this.MainForm_Fill_Panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainForm_Fill_Panel.Location = new System.Drawing.Point(8, 55);
+            this.MainForm_Fill_Panel.Name = "MainForm_Fill_Panel";
+            this.MainForm_Fill_Panel.Size = new System.Drawing.Size(1086, 410);
+            this.MainForm_Fill_Panel.TabIndex = 0;
+            // 
+            // _MainForm_Toolbars_Dock_Area_Left
+            // 
+            this._MainForm_Toolbars_Dock_Area_Left.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this._MainForm_Toolbars_Dock_Area_Left.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(219)))), ((int)(((byte)(255)))));
+            this._MainForm_Toolbars_Dock_Area_Left.DockedPosition = Infragistics.Win.UltraWinToolbars.DockedPosition.Left;
+            this._MainForm_Toolbars_Dock_Area_Left.ForeColor = System.Drawing.SystemColors.ControlText;
+            this._MainForm_Toolbars_Dock_Area_Left.InitialResizeAreaExtent = 8;
+            this._MainForm_Toolbars_Dock_Area_Left.Location = new System.Drawing.Point(0, 55);
+            this._MainForm_Toolbars_Dock_Area_Left.Name = "_MainForm_Toolbars_Dock_Area_Left";
+            this._MainForm_Toolbars_Dock_Area_Left.Size = new System.Drawing.Size(8, 410);
+            this._MainForm_Toolbars_Dock_Area_Left.ToolbarsManager = this.ultToolsbarmgr;
+            // 
+            // _MainForm_Toolbars_Dock_Area_Right
+            // 
+            this._MainForm_Toolbars_Dock_Area_Right.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this._MainForm_Toolbars_Dock_Area_Right.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(219)))), ((int)(((byte)(255)))));
+            this._MainForm_Toolbars_Dock_Area_Right.DockedPosition = Infragistics.Win.UltraWinToolbars.DockedPosition.Right;
+            this._MainForm_Toolbars_Dock_Area_Right.ForeColor = System.Drawing.SystemColors.ControlText;
+            this._MainForm_Toolbars_Dock_Area_Right.InitialResizeAreaExtent = 8;
+            this._MainForm_Toolbars_Dock_Area_Right.Location = new System.Drawing.Point(1094, 55);
+            this._MainForm_Toolbars_Dock_Area_Right.Name = "_MainForm_Toolbars_Dock_Area_Right";
+            this._MainForm_Toolbars_Dock_Area_Right.Size = new System.Drawing.Size(8, 410);
+            this._MainForm_Toolbars_Dock_Area_Right.ToolbarsManager = this.ultToolsbarmgr;
+            // 
+            // _MainForm_Toolbars_Dock_Area_Top
+            // 
+            this._MainForm_Toolbars_Dock_Area_Top.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this._MainForm_Toolbars_Dock_Area_Top.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(219)))), ((int)(((byte)(255)))));
+            this._MainForm_Toolbars_Dock_Area_Top.DockedPosition = Infragistics.Win.UltraWinToolbars.DockedPosition.Top;
+            this._MainForm_Toolbars_Dock_Area_Top.ForeColor = System.Drawing.SystemColors.ControlText;
+            this._MainForm_Toolbars_Dock_Area_Top.Location = new System.Drawing.Point(0, 0);
+            this._MainForm_Toolbars_Dock_Area_Top.Name = "_MainForm_Toolbars_Dock_Area_Top";
+            this._MainForm_Toolbars_Dock_Area_Top.Size = new System.Drawing.Size(1102, 55);
+            this._MainForm_Toolbars_Dock_Area_Top.ToolbarsManager = this.ultToolsbarmgr;
+            // 
+            // _MainForm_Toolbars_Dock_Area_Bottom
+            // 
+            this._MainForm_Toolbars_Dock_Area_Bottom.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this._MainForm_Toolbars_Dock_Area_Bottom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(219)))), ((int)(((byte)(255)))));
+            this._MainForm_Toolbars_Dock_Area_Bottom.DockedPosition = Infragistics.Win.UltraWinToolbars.DockedPosition.Bottom;
+            this._MainForm_Toolbars_Dock_Area_Bottom.ForeColor = System.Drawing.SystemColors.ControlText;
+            this._MainForm_Toolbars_Dock_Area_Bottom.InitialResizeAreaExtent = 8;
+            this._MainForm_Toolbars_Dock_Area_Bottom.Location = new System.Drawing.Point(0, 465);
+            this._MainForm_Toolbars_Dock_Area_Bottom.Name = "_MainForm_Toolbars_Dock_Area_Bottom";
+            this._MainForm_Toolbars_Dock_Area_Bottom.Size = new System.Drawing.Size(1102, 8);
+            this._MainForm_Toolbars_Dock_Area_Bottom.ToolbarsManager = this.ultToolsbarmgr;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(1102, 473);
+            this.Controls.Add(this.MainForm_Fill_Panel);
+            this.Controls.Add(this._MainForm_Toolbars_Dock_Area_Left);
+            this.Controls.Add(this._MainForm_Toolbars_Dock_Area_Right);
+            this.Controls.Add(this._MainForm_Toolbars_Dock_Area_Bottom);
+            this.Controls.Add(this._MainForm_Toolbars_Dock_Area_Top);
             this.Name = "MainForm";
             this.Text = "MailForm";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ultToolsbarmgr)).EndInit();
+            this.MainForm_Fill_Panel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Infragistics.Win.UltraWinToolbars.UltraToolbarsManager ultToolsbarmgr;
+        private Infragistics.Win.Misc.UltraPanel MainForm_Fill_Panel;
+        private Infragistics.Win.UltraWinToolbars.UltraToolbarsDockArea _MainForm_Toolbars_Dock_Area_Left;
+        private Infragistics.Win.UltraWinToolbars.UltraToolbarsDockArea _MainForm_Toolbars_Dock_Area_Right;
+        private Infragistics.Win.UltraWinToolbars.UltraToolbarsDockArea _MainForm_Toolbars_Dock_Area_Bottom;
+        private Infragistics.Win.UltraWinToolbars.UltraToolbarsDockArea _MainForm_Toolbars_Dock_Area_Top;
     }
 }
--- AccountPortal/AccountPortal/MainForm.cs
+++ AccountPortal/AccountPortal/MainForm.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Infragistics.Win.UltraWinToolbars;
 
 namespace Kiran.Accountportal.UI
 {
@@ -14,6 +15,27 @@ namespace Kiran.Accountportal.UI
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            loadform();
+        }
+        private void loadform()
+        {
+            RibbonTab hometab = new RibbonTab("Home");
+            this.ultToolsbarmgr.Ribbon.Tabs.Add(hometab);
+
+            RibbonGroup newgroup = new RibbonGroup("New");
+            this.ultToolsbarmgr.Ribbon.Tabs["Home"].Groups.Add(newgroup);
+
+            //RibbonGroupCollection collection= new RibbonGroupCollection();
+            //collection.Add("Customer");
+
+
+            //FontListTool fontFaceTool = new FontListTool("Fontface");
+            //this.ultToolsbarmgr.Tools.Add(fontFaceTool);
+            //fontgrop.Tools.AddTool(fontFaceTool.Key, false);
         }
     }
 }
